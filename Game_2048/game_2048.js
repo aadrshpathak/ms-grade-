787a90d9f9da4b3a68dbee4e78ee8cd7c2d7f5ad
Pradeep Padmukhi <ppadmukhi28@gmail.com>
2022-02-19 14:51:26 +0530

    created slidLeft functionallity to add on left arroKey

--- Game_2048/game_2048.js
+++ Game_2048/game_2048.js
@@ -8,11 +8,18 @@ window.onload = function () {
 };
 
 function setGame() {
-  let board = [
-    [0, 2, 0, 0],
-    [0, 8, 0, 0],
-    [0, 32, 0, 0],
-    [0, 2048, 0, 0],
+//    board = [
+//     [0, 0, 0, 0],
+//     [0, 0, 0, 0],
+//     [0, 0, 0, 0],
+//     [0, 0, 0, 0],
+//   ];
+
+  board = [
+    [2, 2, 2, 2],
+    [2, 2, 2, 2],
+    [4, 4, 8, 8],
+    [4, 4, 8, 8],
   ];
 
   for (let r = 0; r < rows; r++) {
@@ -38,4 +45,52 @@ function updatTile(tile, num) {
       tile.classList.add("n8192");
     }
   }
+}
+
+
+document.addEventListener('keyup', (e) => {
+    if (e.code == "ArrowLeft") {
+        slideLeft();
+    }
+})
+
+function filterZeros(row) {
+    return row.filter(num => num != 0)      // it will create new array without zeros   
+}
+
+function slide(row) {
+    //[0, 2, 2, 2]
+    row = filterZeros(row);        // it will remove all the zeros from the row - [2, 2, 2]
+
+    //slide functionality
+    for (let i = 0; i < row.length - 1; i++) {
+        if (row[i] == row[i + 1]) {
+            row[i] *= 2;
+            row[i + 1] = 0;
+            score += row[i];     //[2, 2, 2] -> [4, 0, 2]
+        }
+    }
+    row = filterZeros(row);    // [4, 2]
+
+    // adding zeros back to the row at end
+    while (row.length < cols) {
+        row.push(0);
+    }                             //[4, 2, 0, 0]
+    return row 
+}
+
+
+
+function slideLeft() {
+    for (let r = 0; r < rows; r++) {
+        let row = board[r];
+        row = slide(row);
+        board[r] = row;
+
+        for (let c = 0; c < cols; c++) {
+            let tile = document.getElementById(r.toString() + '-' + c.toString());
+            let num = board[r][c];
+            updatTile(tile, num);
+        }
+    }
 }
